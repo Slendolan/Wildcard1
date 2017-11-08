6fff150ef96bde82432c2a7b8e41de233f55d7c2
Slendolan <32447218+Slendolan@users.noreply.github.com>
2017-11-07 17:26:59 -0800

    animations
    animations in progress

--- Platformer Test/Assets/Scripts/PlayerController.cs
+++ Platformer Test/Assets/Scripts/PlayerController.cs
@@ -7,14 +7,35 @@ public class PlayerController : MonoBehaviour {
     public float moveSpeed;
     public float jumpHeight;
 
+    Animator theanimator;
+    private bool isMoving;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    /*
+    private bool IsMoving()
+    {
+        if (GetComponent<Rigidbody2D>().velocity.x != 0)
+            return true;
+        else
+            return false;
+    }
+    */
+
+    // Use this for initialization
+    void Start () {
+        theanimator = gameObject.GetComponent<Animator>();
+    }
 	
 	// Update is called once per frame
 	void Update () {
+
+
+        if (GetComponent<Rigidbody2D>().velocity.x > 0)
+            isMoving = true;
+        else
+            isMoving = false;
+
+        theanimator.SetBool(123, isMoving);
+
 		if(Input.GetKeyDown (KeyCode.Space))
         {
             GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, jumpHeight);
@@ -30,12 +51,6 @@ public class PlayerController : MonoBehaviour {
             GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
         }
     }
-
-    private bool IsGrounded()
-    {
-        if (GetComponent<Rigidbody2D>().moveSpeed.y <= 0)
-        {
-
-        }
-    }
+    
 }
+
